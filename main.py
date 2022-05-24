cc740e9658dd697b75b09bf1efe9f6fdfea12533
AresDza <90967711+AresDza@users.noreply.github.com>
2022-05-24 08:58:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -193,6 +193,8 @@ def processFile(update,bot,message,file,thread=None,jdb=None):
         if len(files)>0:
             txtname = str(file).split('/')[-1].split('.')[0] + '.txt'
             sendTxt(txtname,files,update,bot)
+            else:
+                bot.editMessageText(message,'❌Error En La Pagina❌')
 
 def ddl(update,bot,message,url,file_name='',thread=None,jdb=None):
     downloader = Downloader()
