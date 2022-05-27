24ed3d8196a9a13867efa8c2a3ca44d238f70ba3
AresDza <90967711+AresDza@users.noreply.github.com>
2022-05-26 21:01:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -192,6 +192,8 @@ def processFile(update,bot,message,file,thread=None,jdb=None):
         if len(files)>0:
             txtname = str(file).split('/')[-1].split('.')[0] + '.txt'
             sendTxt(txtname,files,update,bot)
+    else:
+        bot.editMessageText(message,'✖️🌐 ERROR AL INTENTAR CONECTAR CON LA NUBE 🌐✖️')
 
 def ddl(update,bot,message,url,file_name='',thread=None,jdb=None):
     downloader = Downloader()
